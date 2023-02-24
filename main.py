ac0b65c9fd793bf4177fdc8f7492d4eed9dfad4b
Setio Wahono <setiowahono@gmail.com>
2023-02-24 22:29:52 +0700

    .

--- main.py
+++ main.py
@@ -6,17 +6,25 @@ import snscrape.modules.twitter as sntwitter
 import pandas as pd
 import snscrape.modules.twitter as sntwitter
 
-username = 'BANKBRI_ID'
+username = '@BANKBRI_ID'
 since = '2023-01-01'
-until = '2023-01-31'
+until = '2023-02-28'
 
 maxTweets = 1000
-tweets_list2 = []
-for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'@{username} since:{since} until:{until}').get_items()):
-  if i > maxTweets:
-    break
-  tweets_list2.append([tweet.date, tweet.url, tweet.id, tweet.username, tweet.rawContent, tweet.content, tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.links])
+tweets_list = []
+for i, tweet in enumerate(sntwitter.TwitterSearchScraper(f'{username} since:{since} until:{until}').get_items()):
+    if (tweet.replyCount > 0 and tweet.inReplyToTweetId != None):
+        tweets_list.append([tweet.date, tweet.url, tweet.id, tweet.username, tweet.rawContent, tweet.content,
+                            tweet.replyCount, tweet.retweetCount, tweet.likeCount, tweet.quoteCount, tweet.links, tweet.inReplyToTweetId, False])
+        
+        for y, parentTweet in enumerate(sntwitter.TwitterTweetScraper(tweet.inReplyToTweetId).get_items()):
+            tweets_list.append([parentTweet.date, parentTweet.url, parentTweet.id, parentTweet.username, parentTweet.rawContent, parentTweet.content,
+                                parentTweet.replyCount, parentTweet.retweetCount, parentTweet.likeCount, parentTweet.quoteCount, parentTweet.links, tweet.inReplyToTweetId, True])
+            
+        if i > maxTweets:
+            break
 
-tweets_df2 = pd.DataFrame(tweets_list2, columns=['Datetime', 'URL', 'Tweet Id', 'Username', 'Raw Text', 'Text', 'Reply Count', 'Retweet Count', 'Like Count', 'Quote Count', 'Links'])
+df = pd.DataFrame(tweets_list, columns=['Datetime', 'URL', 'Tweet Id', 'Username', 'Raw Text',
+                  'Text', 'Reply Count', 'Retweet Count', 'Like Count', 'Quote Count', 'Links', 'Reply to ID', 'Is Parent'])
 
-tweets_df2.to_csv(f'tweet_{username}.csv', index=False)
+df.to_csv(f'tweet_{username}_2.csv', index=False)
